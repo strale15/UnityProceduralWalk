04210d8c3bd2deb6a2b7bc31fd803c784123dd4b
strale15 <karte32114@gmail.com>
2023-09-11 13:05:08 +0200

    CameraFix

--- Assets/Scripts/StepGiver.cs
+++ Assets/Scripts/StepGiver.cs
@@ -9,6 +9,7 @@ public class StepGiver : MonoBehaviour
     public Transform playerBody;
 
     private float xRotation;
+    private float yRotation;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -21,11 +22,13 @@ public class StepGiver : MonoBehaviour
         //Camera look
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-
+        
         xRotation -= mouseY;
+        yRotation += mouseX;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        playerBody.Rotate(Vector3.up * mouseX);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        playerBody.rotation = Quaternion.Euler(0, yRotation, 0);
+        //playerBody.Rotate(Vector3.up * mouseX);
     }
 }
